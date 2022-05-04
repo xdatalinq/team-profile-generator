a89215f7a6b661e83dacd3997dfe89e9b350e8f5
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-04 15:23:08 -0400

    added engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer.js')
+
+test('create an engineer object', () => {
+    const engineer = new Engineer('Tommy', '9', 'tommy@test.com', 'TommyGithub');
+
+    expect(engineer.github).toBe('TommyGithub');
+});
+
+test('getGithub() function', () => {
+    const engineer = new Engineer('Tommy', '9', 'tommy@test.com', 'TommyGithub');
+
+    expect(engineer.getGithub()).toBe('TommyGithub');
+});
+
+test('getRole() function', () => {
+    const engineer = new Engineer('Tommy', '9', 'tommy@test.com', 'TommyGithub');
+
+    expect(engineer.getRole()).toBe('Engineer');
+});
