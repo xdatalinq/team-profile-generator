dc5ffc0b2d1f629f51357d9525db3b3f52b5ea7b
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-04 14:56:49 -0400

    added employee tests and updated generate-page, installed inquirer and fs

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Tommy', '9', 'tommy@test.com');
+
+    expect(employee.name).toBe('Tommy');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toContain('@');
+    expect(employee.email).toBe('tommy@test.com');
+});
+
+test('getName() function', () => {
+    const employee = new Employee('Tommy', '9', 'tommy@test.com');
+
+    expect(employee.getName()).toBe('Tommy');
+});
+
+test('getID() function', () => {
+    const employee = new Employee('Tommy', '9', 'tommy@test.com');
+
+    expect(employee.getID()).toBe('9');
+});
+
+test('getEmail() function', () => {
+    const employee = new Employee('Tommy', '9', 'tommy@test.com');
+
+    expect(employee.getEmail()).toBe('tommy@test.com');
+});
+
+test('getRole() function', () => {
+    const employee = new Employee('Tommy', '9', 'tommy@test.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
--- src/generate-page.js
+++ src/generate-page.js
@@ -1,43 +1,42 @@
 // generate employees
 const generateEmployees = employeesArr => {
-    if (!employeeArr) {
-        return '';
-    }   
-    return `
-        html here probably divs
-    `;   
-};
+  if (!employeeArr) {
+      return '';
+  }   
+  return `
+      html here probably divs
+  `;   
+}
 
 //generate page
 module.exports = generatePage => {
-    const { employees, ...header } = templateData;
-
-    return `
-    <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>My Team</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-      </head>
-    
-      <body>
-        <header>
-          <div class="container flex-row justify-space-between align-center py-3">
-            <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-          </div>
-        </header>
-        <main class="container my-5">
-          ${generateEmployees(employees)}
-        </main>
-        <footer class="container text-center py-3">
-          <h3></h3>
-        </footer>
-      </body>
-      </html>
-    `;
-};
+  const { employees, ...header } = generatePage;
+  return `
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+  
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateEmployees(employees)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3></h3>
+      </footer>
+    </body>
+    </html>
+  `;
+}
