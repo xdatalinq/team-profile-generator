cf7ace9629138265f2bfa1fac24b91f25b2647b8
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-05-04 18:06:00 -0400

    updated intern test and added intern object with passing tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require('../lib/Intern.js')
 
 test('create an intern object', () => {
-    const intern = new Intern('Tommy', '9', 'tommy@test.com', 'TommyGithub', 'University of Toronto');
+    const intern = new Intern('Tommy', 9, 'tommy@test.com', 'University of Toronto');
 
     expect(intern.school).toBe('University of Toronto');
 });
 
 test('getRole() function', () => {
-    const intern = new Intern('Tommy', '9', 'tommy@test.com', 'TommyGithub', 'University of Toronto');
+    const intern = new Intern('Tommy', 9, 'tommy@test.com', 'University of Toronto');
 
     expect(intern.getRole()).toBe('Intern');
 });
 
 test('getSchool() function', () => {
-    const intern = new Intern('Tommy', '9', 'tommy@test.com', 'TommyGithub', 'University of Toronto');
+    const intern = new Intern('Tommy', 9, 'tommy@test.com', 'University of Toronto');
 
     expect(intern.getSchool()).toBe('University of Toronto');
 });
